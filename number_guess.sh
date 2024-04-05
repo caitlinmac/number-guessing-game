2c9a02a2a6b22de9f643dd7e20449f15fcd6ddac
Caitlin M <caitlin.maceachen@gmail.com>
2024-04-05 16:06:54 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#randomly generate a number
+#users have to guess that number
+## prompt user for username (22 characters or less?)
+## check database for that username
+## display games_played and best_game (measured by number of guesses min)
+## if username doesnt exist, welcome them 
+## play the game: input > higher or lower > new input
+## count number of loops
+## if not an integer, display message
+## when number is guessed, stop counting and display message + store in db
+
