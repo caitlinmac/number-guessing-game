7974539bfe7e9bd2fe7d8ce03f1b2800acf765bf
Caitlin M <caitlin.maceachen@gmail.com>
2024-04-05 18:50:16 +0000

    feat: username request and check

--- number_guess.sh
+++ number_guess.sh
@@ -8,18 +8,20 @@ echo $RANDOM_NO
 ## prompt user for username
 echo Enter your username:
 read USERNAME_ENTERRED
+echo enterred $USERNAME_ENTERRED
 ## check database for that username
-USERNAME_CHECK=$($PSQL "SELECT username, games_played, best_game FROM players_stats WHERE username='$USERNAME_ENTERRED';")
-IFS='|'
-echo $USERNAME_CHECK | while read USERNAME GAMES_PLAYED BEST_GAME; do
-echo $USERNAME $GAMES_PLAYED $BEST_GAME
+USERNAME_CHECK=$($PSQL "SELECT * FROM players_stats WHERE username='$USERNAME_ENTERRED';")
+  IFS=$'|'
+echo "$USERNAME_CHECK" | while read USER_ID USERNAME GAMES_PLAYED BEST_GAME
+do
+echo "new var" $USERNAME $GAMES_PLAYED $BEST_GAME
 done
 
 if [[ -z $USERNAME_CHECK ]]
 then
 #if username query is blank
-#add user to db
-ADD_NEW_USER=$($PSQL "INSERT INTO players_stats(username) VALUES ('$USERNAME_ENTERRED');")
+#add user to db <-- HAVING ISSUES WITH THIS LINE!!!!!
+ADD_NEW_USER=$($PSQL "INSERT INTO players_stats(username) VALUES('$USERNAME_ENTERRED');")
 #welcome message to new user
 echo Welcome, $USERNAME_ENTERRED! It looks like this is your first time here. 
 #if username query not blank, welcome back
